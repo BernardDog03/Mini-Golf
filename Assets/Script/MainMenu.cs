b99bdb7804963563d907b0581315a4475c6e49da
BernardDog03 <rafifsuja03@gmail.com>
2023-05-16 13:05:24 +0700

    Add Menu Section

---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+    public void SettingPage()
+    {
+        SceneManager.LoadScene("Setting");
+    }
+    public void CloseGame()
+    {
+        Application.Quit();
+    }
+}
